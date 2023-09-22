edb75df004d18a104f26fcb698715874fda58942
Gabriel Flores <108844543+GabrielFlores8227@users.noreply.github.com>
2023-09-22 17:47:13 -0300

    update

--- src/router/root-api/root-api.ts
+++ src/router/root-api/root-api.ts
@@ -7,6 +7,11 @@ import FastLog from 'ks-fastlog';
 
 const rootDirectory = execSync('pwd', { encoding: 'utf-8' }).trim();
 
+const saaEmailContent = fs.readFileSync(
+	path.join(rootDirectory, 'views/saa-email/index.html'),
+	'utf8',
+);
+
 const clientEmailContent = fs.readFileSync(
 	path.join(rootDirectory, 'views/client-email/index.html'),
 	'utf8',
@@ -26,6 +31,17 @@ rootApi.post(
 				[email],
 				'Trail Life: Vimos que você está interessado em nossos serviços.',
 				clientEmailContent,
+			);
+
+			await Mailer.sendEmail(
+				process.env.AWS_SES_SOURCE_EMAIL!,
+				[process.env.AWS_SES_SAA_EMAIL!],
+				'Trail Life: Um novo cliente em potencial está solicitando informações.',
+				saaEmailContent
+					.replace('!name!', name)
+					.replace('!phone!', phone)
+					.replace('!email!', email)
+					.replace('!message!', message || 'Não Especificadas'),
 			);
 		} catch (err: any) {
 			FastLog.log(err.message, 'error');
